13baf84ac896c7b3aa6eb8b205c36c6fddc2d305
Yankho <ysaliji023@student.wethinkcode.co.za>
2024-10-24 20:12:29 +0200

    added random colours

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseover', changeColorOnHover);
     grid.appendChild(cell);
   }
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColorOnHover(event) {
+    event.target.style.backgroundColor = getRandomColor();
 }
